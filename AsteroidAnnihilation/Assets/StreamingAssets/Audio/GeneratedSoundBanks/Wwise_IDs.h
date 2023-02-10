d493205b071c8366882de1488c33d8e2b5186213
Nino Saglia <nino.saglia97@gmail.com>
2023-02-10 11:12:57 +0100

    Added audio hierarchy for the spaceship and a bunch of RTPC's

--- AsteroidAnnihilation/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ AsteroidAnnihilation/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,19 +13,58 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_ETFX_SHOOT_LASER = 2574519627U;
-        static const AkUniqueID PLAY_LEVELUP = 1202337555U;
-        static const AkUniqueID PLAY_PLASMAGUNSHOT1 = 1317655475U;
-        static const AkUniqueID PLAY_PLASMAGUNSHOT2 = 1317655472U;
-        static const AkUniqueID PLAY_PLASMAGUNSHOT3 = 1317655473U;
-        static const AkUniqueID PLAY_PLASMAGUNSHOT4 = 1317655478U;
-        static const AkUniqueID PLAY_SWARMEXPLOSION = 4211333239U;
+        static const AkUniqueID TEMP_PLAY_ETFX_SHOOT_LASER = 3201422434U;
+        static const AkUniqueID TEMP_PLAY_LEVELUP = 1450272620U;
+        static const AkUniqueID TEMP_PLAY_PLASMAGUNSHOT1 = 3965960414U;
+        static const AkUniqueID TEMP_PLAY_PLASMAGUNSHOT2 = 3965960413U;
+        static const AkUniqueID TEMP_PLAY_PLASMAGUNSHOT3 = 3965960412U;
+        static const AkUniqueID TEMP_PLAY_PLASMAGUNSHOT4 = 3965960411U;
+        static const AkUniqueID TEMP_PLAY_SWARMEXPLOSION = 2360230854U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace PLAYERSHIPGUNLEVEL
+        {
+            static const AkUniqueID GROUP = 2663975140U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID HIGH = 3550808449U;
+                static const AkUniqueID LOW = 545371365U;
+                static const AkUniqueID MED = 981339021U;
+            } // namespace SWITCH
+        } // namespace PLAYERSHIPGUNLEVEL
+
+        namespace PLAYERSHIPHEALTHSWITCH
+        {
+            static const AkUniqueID GROUP = 2792155054U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ALMOSTDEAD = 777824873U;
+                static const AkUniqueID DEAD = 2044049779U;
+                static const AkUniqueID FULL = 2510516222U;
+                static const AkUniqueID LOW = 545371365U;
+                static const AkUniqueID MEDIUM = 2849147824U;
+            } // namespace SWITCH
+        } // namespace PLAYERSHIPHEALTHSWITCH
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PLAYERSHIPENERGY = 2508725640U;
+        static const AkUniqueID PLAYERSHIPGUNLEVEL = 2663975140U;
+        static const AkUniqueID PLAYERSHIPHEALTH = 1438366550U;
+        static const AkUniqueID PLAYERSHIPTURNRATE = 1347618923U;
+        static const AkUniqueID PLAYERSHIPVELOCITY = 3228488565U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID BASE_SOUNDBANK = 3149804152U;
+        static const AkUniqueID PERSISTENT_SOUNDBANK = 1299072232U;
     } // namespace BANKS
 
     namespace BUSSES
